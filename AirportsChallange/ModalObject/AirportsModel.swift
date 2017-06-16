896c3252b3785d8c2b0028ba05c1b83182530c92
Sravan Kumar <sravan.kumar2@honeywell.com>
2017-06-16 14:46:16 +0530

    new files added

---
+++ AirportsChallange/ModalObject/AirportsModel.swift
@@ -0,0 +1,13 @@
+//
+//  AirportsModel.swift
+//  AirportsChallange
+//
+//  Created by Sravan Kumar on 16/06/17.
+//  Copyright © 2017 com.honeywell. All rights reserved.
+//
+
+import UIKit
+
+class AirportsModel: NSObject {
+
+}
