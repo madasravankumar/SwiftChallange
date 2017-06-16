35c3aba3977c840b8f21f4a4cf85e8d41ba9307d
Kranthi Kumar <Kranthi@kranthis-mbp.honeywell.com>
2017-06-16 14:46:42 +0530

    Bridge HEader

---
+++ AirportsChallange/AirportDataManager.swift
@@ -0,0 +1,13 @@
+//
+//  AirportDataManager.swift
+//  AirportsChallange
+//
+//  Created by Kranthi Kumar on 16/06/17.
+//  Copyright © 2017 com.honeywell. All rights reserved.
+//
+
+import Foundation
+
+class AirportDataManager : NSObject {
+    
+}
